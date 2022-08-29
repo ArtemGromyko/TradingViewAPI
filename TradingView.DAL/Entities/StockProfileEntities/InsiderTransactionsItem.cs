e96652df954317fd1a53e14e48345dd747fd9522
Anton Volchkov <anton.volchkov@diflexmo.be>
2022-08-29 13:00:43 +0300

    fix data type in entity InsiderTransactionsItem

--- TradingView.DAL/Entities/StockProfileEntities/InsiderTransactionsItem.cs
+++ TradingView.DAL/Entities/StockProfileEntities/InsiderTransactionsItem.cs
@@ -1,26 +1,26 @@
 ﻿namespace TradingView.DAL.Entities.StockProfileEntities;
 public class InsiderTransactionsItem
 {
-    public string ConversionOrExercisePrice { get; set; }
-    public string DirectIndirect { get; set; }
-    public string EffectiveDate { get; set; }
+    public double? ConversionOrExercisePrice { get; set; }
+    public char DirectIndirect { get; set; }
+    public int EffectiveDate { get; set; }
     public string FilingDate { get; set; }
     public string FullName { get; set; }
-    public string Is10b51 { get; set; }
-    public string PostShares { get; set; }
+    public bool Is10b51 { get; set; }
+    public int PostShares { get; set; }
     public string ReportedTitle { get; set; }
     public string Symbol { get; set; }
-    public string TransactionCode { get; set; }
+    public char TransactionCode { get; set; }
     public string TransactionDate { get; set; }
-    public string TransactionPrice { get; set; }
-    public string TransactionShares { get; set; }
-    public string TransactionValue { get; set; }
+    public double TransactionPrice { get; set; }
+    public double TransactionShares { get; set; }
+    public double TransactionValue { get; set; }
     public string Id { get; set; }
     public string Key { get; set; }
     public string Subkey { get; set; }
     public string Date { get; set; }
-    public string Updated { get; set; }
-    public string TranPrice { get; set; }
-    public string TranShares { get; set; }
-    public string TranValue { get; set; }
+    public int Updated { get; set; }
+    public double TranPrice { get; set; }
+    public double TranShares { get; set; }
+    public double TranValue { get; set; }
 }
