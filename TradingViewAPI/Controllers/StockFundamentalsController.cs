806b53b79ca99da11ff5a5be9c99408664cc0f89
Anton Volchkov <anton.volchkov@diflexmo.be>
2022-09-06 12:29:50 +0300

    added controller

---
+++ TradingViewAPI/Controllers/StockFundamentalsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TradingViewAPI.Controllers;
+[Route("api")]
+[ApiController]
+public class StockFundamentalsController : ControllerBase
+{
+    public StockFundamentalsController()
+    {
+
+    }
+}
