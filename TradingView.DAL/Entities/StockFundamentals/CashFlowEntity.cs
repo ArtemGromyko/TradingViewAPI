d1345a625dbdab7efb069a27df1771aafe2ff010
Anton Volchkov <anton.volchkov@diflexmo.be>
2022-09-06 16:08:11 +0300

    added entity

---
+++ TradingView.DAL/Entities/StockFundamentals/CashFlowEntity.cs
@@ -0,0 +1,6 @@
+﻿namespace TradingView.DAL.Entities.StockFundamentals;
+public class CashFlowEntity : EntityBase
+{
+    public string Symbol { get; set; }
+    public List<CashFlowItem> CashFlow { get; set; }
+}
